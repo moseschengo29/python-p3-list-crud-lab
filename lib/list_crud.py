8aad4911802039a934d23d5ace248a5be818d25a
Moses <mochengo29@gmail.com>
2023-08-21 21:55:57 +0300

    Completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,39 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ['moses', 'martin', 'grace', 'john']
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    return l
+
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop()
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    return l[1:]
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
+
+
+arr = [1,2,3,4]
+print(arr[1:])
+
+
+pple = create_a_list()
+fam = add_element_to_start_of_list(pple, 'judith')
+print(fam)
